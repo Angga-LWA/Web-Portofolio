c4041bec5ea4d06bcf662996922598fa962a3e6d
Angga-LWA <69418956+Angga-LWA@users.noreply.github.com>
2022-06-25 15:19:04 +0700

    update index.html
    update landpage

--- YogaBlog/Frontend/js/main.js
+++ YogaBlog/Frontend/js/main.js
@@ -114,4 +114,28 @@
 		}
 	});
 
+	/*------------------
+       logo Carousel
+    --------------------*/
+    $(".logo-carousel").owlCarousel({
+        loop: false,
+        margin: 30,
+        nav: false,
+        items: 5,
+        dots: false,
+        navText: ['<i class="ti-angle-left"></i>', '<i class="ti-angle-right"></i>'],
+        smartSpeed: 1200,
+        autoHeight: false,
+        mouseDrag: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 3,
+            },
+            768: {
+                items: 5,
+            }
+        }
+    });
+
 })(jQuery);
